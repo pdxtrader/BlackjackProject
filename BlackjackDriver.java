35adee158a24882f02f07e22e301fbfd5da636f4
pdxtrader <benjaminblodgett@gmail.com>
2017-04-23 16:31:45 -0600

    60%

--- BlackjackDriver.java
+++ BlackjackDriver.java
@@ -25,6 +25,7 @@ public class BlackjackDriver {
 		d.newDeck();
 		
 		//MAKE YOUR WAGER LOGIC
+			//all in: you boldly push your pile of beaver pelts to the middle of the table
 		
 		//Dealer deals initial hand 
 		p.addCard(d.dealCard());	//passes a card from the deck to the player
@@ -33,11 +34,14 @@ public class BlackjackDriver {
 		d.addCard(d.dealCard());	//passes a card from the deck to the dealer
 		
 		//Instantiates hand sums for dealer and player
-		int dealerTotal = driver.calculateHand(d.getDealersHand()); 
+		int dealerTotal = driver.calculateHand(d.getDealersHand());
 		int playerTotal = driver.calculateHand(p.getPlayersHand());
-	
+		
+		//Begins hand loop
+		while (dealerTotal < 21 && playerTotal < 21) {  //or do a recursive call back to the top
+			
 		//Initial hand messaging
-		System.out.println("Dealer is showing: [" + d.getDealersHand().get(1) + "]");   //******IF DEALER IS SHOWING ACE SKIP DECISION LOGIC
+		System.out.println("Dealer is showing: [" + d.getDealersHand().get(1) + "]" );   //******IF DEALER IS SHOWING ACE SKIP DECISION LOGIC
 		System.out.println("Your hand contains: " + p.getPlayersHand());
 		
 		//Initial decision logic  MOVE INTO PLAYER METHOD
@@ -45,6 +49,7 @@ public class BlackjackDriver {
 		System.out.println("1) Hit");
 		System.out.println("2) Stay");
 		
+		//possibly move this to logic in the dealer class
 		int doWhat = kb.nextInt();
 		
 		switch (doWhat) {
@@ -54,9 +59,27 @@ public class BlackjackDriver {
 				System.out.println("Your hand now contains: " + p.getPlayersHand());
 				break;
 			case 2:
+				System.out.println("meow");
+				break;
 				
 		}
-		
+			//Updates hand totals after an hitting, staying, etc
+			dealerTotal = driver.calculateHand(d.getDealersHand());  
+			System.out.println(dealerTotal);
+			playerTotal = driver.calculateHand(p.getPlayersHand());
+			System.out.println(playerTotal);
+			
+			//enter if logic 
+			if (playerTotal > 21) {
+				System.out.println("You have busted!!");
+//				System.out.println("The dealer collects your " + XXX + " pelts");
+				//minus your beaver pelts
+			}
+				else if (dealerTotal > 21) {
+					System.out.println("The dealer has busted!!");
+//					System.out.println("The dealer slaps " + XXX + " pelts on the table");
+			}
+		}
 		
 		//End comparison logic/Prints out sums
 
--- Rank.java
+++ Rank.java
@@ -2,7 +2,7 @@ package blackjack;
 
 public enum Rank {
 
-		Ace(1), Two(2), Three(3), Four(4), Five(5), Six(6), Seven(7), Eight(8),
+		Ace(11), Two(2), Three(3), Four(4), Five(5), Six(6), Seven(7), Eight(8),
 		Nine(9), Ten(10), Jack(10), Queen(10), King(10);
 
 		private int numVal;
